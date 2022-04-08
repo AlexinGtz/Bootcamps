46073dfed1909e10f5d5b2df13a0b34c776f6c2f
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-04-07 23:28:39 -0500

    Fixed error in put item line

--- Clase 1 SAM/src/app.py
+++ Clase 1 SAM/src/app.py
@@ -1,13 +1,14 @@
 import json
+import uuid
 import boto3
 
 dynamodb = boto3.client('dynamodb', region_name='us-west-1')
 
 def lambda_handler(event, context):
 
-    returnedVal = dynamodb.putItem(TableName='users', Item= {
+    returnedVal = dynamodb.put_item(TableName='users', Item= {
         'id': {
-            'S': '1234Fr'
+            'S': uuid.uuid4()
         },
         'name': {
             'S': 'Alex'
