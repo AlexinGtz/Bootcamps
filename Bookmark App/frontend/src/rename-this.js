2b873e1539a3b405e2c12b961dfdc55768da1290
Alejandro Gutierrez <j.alex1410@hotmail.com>
2022-04-21 22:56:46 -0500

    Keep fixing files

---
+++ Bookmark App/frontend/src/rename-this.js
@@ -0,0 +1,16 @@
+// RENAME THIS TO aws-exports.js
+const awsmobile = {
+    "aws_project_region": "(REGION)",
+    "aws_cognito_region": "(REGION)",
+    "aws_user_pools_id": "(POOL_ID)",
+    "aws_user_pools_web_client_id": "(APP_CLIENT_ID)",
+    "oauth": {},
+    "aws_cloud_logic_custom": [
+        {
+            "name": "Bookmark App",
+            "endpoint": "(API_URL)",
+            "region": "(REGION)"
+        }
+    ]
+};
+    export default awsmobile;
